0bf7f7307054a38572f32b5d7e828a53b15db5d5
anmendeza <andrea@firstfactory.com>
2021-05-20 20:23:32 -0600

    hello world commit

--- OOP/src/com/ucreativa/Main.java
+++ OOP/src/com/ucreativa/Main.java
@@ -3,9 +3,6 @@ package com.ucreativa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Hello world");
     }
 }
-
-
-//I need to know where this was pushed :/
