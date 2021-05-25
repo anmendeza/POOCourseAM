379da7155522a4aae73a414ef74c25e8c2d9455b
anmendeza <andrea@firstfactory.com>
2021-05-24 20:06:19 -0600

    Trabajo en clase mayo 24

--- OOP/src/com/ucreativa/Main.java
+++ OOP/src/com/ucreativa/Main.java
@@ -1,12 +1,17 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Andrea;
+import com.ucreativa.familia.Yoryi;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
         Andrea smart = new Andrea();
-
     }
+
+
+    Yoryi loving = new Yoryi();
+
 }
---
+++ OOP/src/com/ucreativa/familia/Yoryi.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class Yoryi {
+
+    public Yoryi() {
+        System.out.println("Hola mamita como estas?");
+    }
+}
